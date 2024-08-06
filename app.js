4bfb4cc8de2aecaeed65459adf83df1eebc62b7e
Samuel <samuferfort@gmail.com>
2024-08-06 16:44:14 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config()
+const express = require("express")
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on port http://localhost:${PORT}`)
+})
